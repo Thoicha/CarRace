fb0ca76eb30e33f954b4bfb6b1bebdbb3caccc3f
Iris <69468311+Thoicha@users.noreply.github.com>
2020-10-05 18:39:19 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,31 @@
+class Form{
+    constructor(){
+        this.input = createInput("name");
+        this.button = createButton("play");
+        this.greeting = createElement('h3');
+    }
+
+    hide(){
+        this.input.hide();
+        this.button.hide();
+        this.greeting.hide();
+    }
+    display(){
+        var title = createElement('h2');
+        title.html("Car Racing Game");
+        title.position(130,0);       
+        this.input.position(130,160);
+        this.button.position(200,200);
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name =this.input.value();
+            player.update();
+            playerCount+=1;
+            player.updateCount(playerCount);
+            player.index = playerCount;
+            this.greeting.html("Hello "+player.name);
+            this.greeting.position(100,200);
+        });
+    }
+}
---
+++ Game.js
@@ -0,0 +1,55 @@
+class Game{
+    constructor(){
+
+    }
+    getState(){
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value",function(data){
+            gameState = data.val();
+        });        
+    }
+    update(state){
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    
+    async start(){
+        if(gameState === 0){
+            player = new Player();
+
+            var playerCountRef = await database.ref('playerCount').once("value");
+            if (playerCountRef.exists()){
+                playerCount = playerCountRef.val();
+                player.getCount();
+
+            }
+
+            form = new Form();
+            form.display();
+        }
+    }
+    play(){
+        form.hide();
+        textSize(30);
+        text("Game Start",120,100);
+        Player.getPlayerInfo();
+        if(allPlayers!==undefined){
+            var posY = 130;
+            for(var p in allPlayers){
+                textSize(15);
+                if(p==="player"+player.index)
+                    fill("red");
+                else
+                    fill("black");
+                    
+                text(allPlayers[p].name+": "+allPlayers[p].distance,120,posY);
+                posY+=20;
+            }
+        }
+        if(keyIsDown(UP_ARROW)&&this.player.index!==null){
+            player.distance+=50;
+            player.update();
+        }
+    }
+}
